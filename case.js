282739f713b82de2b5396225e79a7962350ea0cc
Badsha012 <badshasheikh361@gmail.com>
2025-07-31 08:20:18 -0700

    five commit

---
+++ case.js
@@ -0,0 +1,23 @@
+const school="Khulna zill school Khulna";
+console.log(school);
+
+// to lowercase 
+
+
+console.log(school.toLowerCase());
+// to upper case 
+console.log(school.toUpperCase())
+//uercase  :- ABCD EFJ H
+//lowercase :-abcd egg h
+
+const subject='Chemistry';
+const book='chemistry';
+if(subject==book){
+    console.log(' i am reagingber exam fitaia dibo');
+
+}
+else{
+    console.log('udai hudai ultai vat ady oppaka kori');
+
+}
+
