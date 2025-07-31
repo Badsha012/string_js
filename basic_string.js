54bdbfcd2afa24bc74c0dc3a85b16cc53949bd0d
Badsha012 <badshasheikh361@gmail.com>
2025-07-31 08:03:43 -0700

    two commit

--- basic_string.js
+++ basic_string.js
@@ -6,4 +6,21 @@ const thana= new String('Rampal');
 console.log(thana);
 
 
-console.log(country);
+// const numbeOrs=[2,8,6,8,10,12,14,16,18];
+// console.log(numbeOrs.length);
+// console.log(numbeOrs[1]);
+
+
+const capital='dhaka';
+// console.log(capital.length);
+// console.log(capital[2]);
+console.log(capital);
+
+// strig is immmutanle nochanged
+// array is immutable is change
+capital[0]='R';
+console.log(capital);
+
+
+
+// console.log(country);
