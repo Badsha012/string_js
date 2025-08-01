abd6d55164dffbf60fe749c5412101f5ef9bdf73
Badsha012 <badshasheikh361@gmail.com>
2025-08-01 02:17:30 -0700

    five commit

---
+++ object.js
@@ -0,0 +1,33 @@
+const school='mother school';
+  
+const age =23;
+const Ispassed=true;
+let Isdev;
+const subject='bangla,math physics,cheitry';
+
+const bottle_wrong=['water',45];
+const bottleColor='White';
+const bottlePrice=45;
+const bottleDrinks='water';
+
+//bj
+const bottle={
+    brand:'apple',
+    price:45,
+    color:'white',
+    isClean:false
+}
+const sub={
+    name:'biology',
+    teacher:'nila',
+    exam:'30 feb',
+    chapter:['frist','second','four'],
+    exams :{
+        name:'final Exam',
+        marks:100
+
+    }
+}
+
+
+console.log(Isdev);
