383b4f6023666a6862feceb0ed6645fe5075ea05
Badsha012 <badshasheikh361@gmail.com>
2025-07-31 20:19:10 -0700

    four commit

---
+++ conct.js
@@ -0,0 +1,8 @@
+const first='abid';
+const last='kabit';
+// const fullName=first+last;
+
+// const fullName =first+' ' +last;
+
+const fullName=first.concat(' ').concat(last);
+console.log(fullName);
