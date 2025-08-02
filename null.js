2dd0beb407d734b9fabed9004cd7a2600dc3a418
Badsha012 <badshasheikh361@gmail.com>
2025-08-02 16:30:39 -0700

    nine commmit

--- null.js
+++ null.js
@@ -1,2 +1,9 @@
 let x;
 console.log(x);
+
+
+let a=null;
+console.log(a);
+
+
+
---
+++ property.js
@@ -0,0 +1,11 @@
+
+const person={
+   name:'sodor uddin',
+   age:25,
+   profession:'developer',
+   salary:40000,
+   married:true
+
+
+}
+console.log(person);
